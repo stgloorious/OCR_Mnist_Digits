8823412491fa29aaedb2e5ce4da17de60d52588a
Stefan Gloor <48062150+stgloorious@users.noreply.github.com>
2020-08-24 23:57:42 +0200

    updated docs

--- doc/html/search/all_8.js
+++ doc/html/search/all_8.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['test_13',['test',['../namespacetest.html',1,'test'],['../namespacetest.html#ac907a916d990e7caf577a1fbea16d7f2',1,'test.test()']]],
   ['test_2epy_14',['test.py',['../test_8py.html',1,'']]],
-  ['train_15',['train',['../namespacetrain.html',1,'train'],['../classnn_1_1neural_network.html#a4fa8c0fbdac46cd8412b963289615897',1,'nn.neuralNetwork.train()'],['../namespacetrain.html#a92ebd1bae11f8c6a2e181954dabb6c9b',1,'train.train()']]],
+  ['train_15',['train',['../namespacetrain.html',1,'train'],['../classnn_1_1neural_network.html#a4fa8c0fbdac46cd8412b963289615897',1,'nn.neuralNetwork.train()'],['../namespacetrain.html#a1f98e6b87c1f0e1baf3481b4036291ee',1,'train.train()']]],
   ['train_2epy_16',['train.py',['../train_8py.html',1,'']]]
 ];
--- doc/html/search/functions_2.js
+++ doc/html/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['test_30',['test',['../namespacetest.html#ac907a916d990e7caf577a1fbea16d7f2',1,'test']]],
-  ['train_31',['train',['../classnn_1_1neural_network.html#a4fa8c0fbdac46cd8412b963289615897',1,'nn.neuralNetwork.train()'],['../namespacetrain.html#a92ebd1bae11f8c6a2e181954dabb6c9b',1,'train.train()']]]
+  ['train_31',['train',['../classnn_1_1neural_network.html#a4fa8c0fbdac46cd8412b963289615897',1,'nn.neuralNetwork.train()'],['../namespacetrain.html#a1f98e6b87c1f0e1baf3481b4036291ee',1,'train.train()']]]
 ];
