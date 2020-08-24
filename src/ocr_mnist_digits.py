cc6eebe657f0d9dce6f343c3c609ed53a46e97d9
Stefan Gloor <48062150+stgloorious@users.noreply.github.com>
2020-08-24 23:45:52 +0200

    Added multiple epoch functionality, tweaked parameters

--- src/ocr_mnist_digits.py
+++ src/ocr_mnist_digits.py
@@ -7,12 +7,12 @@
 import nn
 
 # CREATE INSTANCE OF NEURAL NETWORK
-n = nn.neuralNetwork((28*28),100,10,0.3)
-# input is a 28 by 28 image, 100 hidden layers. 
-# output are digit indicators 0...9. Learning rate is 0.3.
+n = nn.neuralNetwork((28*28),500,10,0.1)
+# input is a 28 by 28 image, 500 hidden layers. 
+# output are digit indicators 0...9. Learning rate is 0.1.
 
 # TRAIN WITH MNIST DATABASE (60'000 samples)
-nn.train.train(n,"C:/Users/stefa/Desktop/mnist_train.csv") # <-- Change path
+nn.train.train(n,"C:/Users/stefa/Desktop/mnist_train.csv",8) # <-- Change path
 
 # TEST WITH SEPARATE TEST DATA SET (10'000 samples)
 nn.test.test(n,"C:/Users/stefa/Desktop/mnist_test.csv") # <-- Change path
--- src/train.py
+++ src/train.py
@@ -13,8 +13,9 @@ from tqdm import tqdm
 ## Trains the neural network
 #   @param neural_network The neural network object that should be trained
 #   @param training_data_path Path to the .csv file that contains the training data. First entry must be label.
+#   @param epochs Number of iterations the whole training set is used for training (repeated training)
 #
-def train (neural_network, training_data_path):
+def train (neural_network, training_data_path, epochs):
 
     ## ACQUIRE TRAINING DATA FROM DATABASE
     print("Getting training data...")
@@ -31,21 +32,24 @@ def train (neural_network, training_data_path):
 
     # prepare progress bar
     progress=0
-    maxValue=len(training_data_list)
-    pbar = tqdm(total=maxValue,desc="Training")
+    maxValue=len(training_data_list)*epochs
+    pbar = tqdm(total=maxValue,desc="Training (%d epochs)" %(epochs))
 
-    # go through all records in the training data
-    for record in training_data_list:
-        #separate by commas
-        all_values = record.split(',')
-        #scale and shift inputs
-        inputs = (numpy.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
-        #create the target output values 
-        targets = numpy.zeros(10) + 0.01
-        #all_values[0] is the target label
-        targets[int(all_values[0])] = 0.99
-        neural_network.train(inputs,targets)
-        pbar.update()
-    pass  
+    # go trough all epochs (repeating the same training)
+    for current_epoch in range(1,epochs,1):
+        # go through all records in the training data
+        for record in training_data_list:
+            #separate by commas
+            all_values = record.split(',')
+            #scale and shift inputs
+            inputs = (numpy.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
+            #create the target output values 
+            targets = numpy.zeros(10) + 0.01
+            #all_values[0] is the target label
+            targets[int(all_values[0])] = 0.99
+            neural_network.train(inputs,targets)
+            pbar.update()
+        pass  
+    pass
     pbar.close()
 pass
