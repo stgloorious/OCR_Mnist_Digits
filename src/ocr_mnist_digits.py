9a71c113997a4a11afb2af8c641ea75db5d311ac
Stefan Gloor <48062150+stgloorious@users.noreply.github.com>
2020-08-25 00:03:50 +0200

    Minor changes

--- src/ocr_mnist_digits.py
+++ src/ocr_mnist_digits.py
@@ -6,13 +6,18 @@
  
 import nn
 
-# CREATE INSTANCE OF NEURAL NETWORK
+## CREATE INSTANCE OF NEURAL NETWORK
+# input is a 28 by 28 image, 500 hidden nodes. 
+# Output is digit indicator 0...9. Learning rate is 0.1.
+# Training process is repeated 10 times (epoches)
 n = nn.neuralNetwork((28*28),500,10,0.1)
-# input is a 28 by 28 image, 500 hidden layers. 
-# output are digit indicators 0...9. Learning rate is 0.1.
 
-# TRAIN WITH MNIST DATABASE (60'000 samples)
-nn.train.train(n,"C:/Users/stefa/Desktop/mnist_train.csv",8) # <-- Change path
+## TRAIN WITH MNIST DATABASE (60'000 samples)
+# Training data: http://www.pjreddie.com/media/files/mnist_train.csv
+#
+nn.train.train(n,"C:/Users/stefa/Desktop/mnist_train.csv",8) # <---- Change this path
 
-# TEST WITH SEPARATE TEST DATA SET (10'000 samples)
-nn.test.test(n,"C:/Users/stefa/Desktop/mnist_test.csv") # <-- Change path
+## TEST WITH SEPARATE TEST DATA SET (10'000 samples)
+# Testing data: http://pjreddie.com/media/files/mnist_test.csv
+#
+nn.test.test(n,"C:/Users/stefa/Desktop/mnist_test.csv") # <---- Change this path
