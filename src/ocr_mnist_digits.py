a95afa512099098c2bc95e2c6f10c33ee14d8d8e
Stefan Gloor <48062150+stgloorious@users.noreply.github.com>
2020-08-26 09:03:27 +0200

    Trained network with large merged data set
    Trained with a merged set from 60000 handwritten and 60000 computer generated sets. Took about 1 h.

--- src/ocr_mnist_digits.py
+++ src/ocr_mnist_digits.py
@@ -9,7 +9,7 @@ import nn
 ## CREATE INSTANCE OF NEURAL NETWORK
 # input is a 28 by 28 image, 500 hidden nodes. 
 # Output is digit indicator 0...9. Learning rate is 0.1.
-n = nn.neuralNetwork((28*28),500,10,0.1)
+n = nn.neuralNetwork((28*28),1000,10,0.1)
 
 ## TRAIN WITH MNIST DATABASE (60'000 samples)
 # Training data: http://www.pjreddie.com/media/files/mnist_train.csv
@@ -20,13 +20,13 @@ n = nn.neuralNetwork((28*28),500,10,0.1)
 #
 # vvv Uncomment next line to train neural network vvv
 #
-nn.train.train(n,"C:/Users/stefa/Dropbox/Documents/Hobby/Software/Python/DigitDatasetCreator/merged_training.csv",3) # <---- Change this path
+nn.train.train(n,"C:/Users/stefa/Dropbox/Documents/Hobby/Software/Python/DigitDatasetCreator/merged_training.csv",5) # <---- Change this path
 
 ## TEST WITH SEPARATE TEST DATA SET (10'000 samples)
 # Testing data: http://pjreddie.com/media/files/mnist_test.csv
 #
 #
-nn.test.test(n,"C:/Users/stefa/Desktop/mnist_test.csv") # <---- Change this path
+nn.test.test(n,"C:/Users/stefa/Dropbox/Documents/Hobby/Software/Python/OCR_Mnist_Digits/data/mnist_test.csv") # <---- Change this path
 
-nn.test.test(n,"C:/Users/stefa/Dropbox/Documents/Hobby/Software/Python/DigitDatasetCreator/testing.csv") # <---- Change this path
+nn.test.test(n,"C:/Users/stefa/Dropbox/Documents/Hobby/Software/Python/DigitDatasetCreator/data/testing.csv") # <---- Change this path
 
